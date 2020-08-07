3973e18622055665c4e5b72a6e67f94120fd5cae
Swapnil Magar <magarswapnil896@gmail.com>
2020-08-08 01:44:04 +0530

    Minor change

---
+++ GithubAPIAssignment/GithubAPIAssignment/ViewModels/UserViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  UserViewModel.swift
+//  GithubAPIAssignment
+//
+//  Created by Swapnil  Magar on 08/08/20.
+//  Copyright © 2020 Swapnil Magar. All rights reserved.
+//
+
+import Foundation
+
+class UserViewModel {
+    
+    //MARK:- Singleton
+    
+    static let shared = UserViewModel()
+    
+    //MARK:- Initialization
+    
+    private init() { }
+    
+}
+
+//MARK:- Public
+
+extension UserViewModel {
+    
+    func getGithubUsers() {
+        
+    }
+    
+}
+
